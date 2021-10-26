390f21d0a39c490c585ac0c68dfc5cb009d2fbba
Aday <aday69@hotmail.com>
2021-10-26 12:32:50 +0100

    Add new routes

--- pokedex-app/frontend/src/app/app-routing.module.ts
+++ pokedex-app/frontend/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'pokedex', loadChildren: () => import('./modules/pokemons/pokemons.module').then((m) => m.PokemonsModule) },
+  { path: '', loadChildren: () => import('./modules/pokemons/pokemons.module').then((m) => m.PokemonsModule) },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
