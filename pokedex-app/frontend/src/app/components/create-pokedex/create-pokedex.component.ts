f2c2e76e8c0883fa91cd313c9bd75850201f1c14
Aday <aday69@hotmail.com>
2021-11-03 10:41:44 +0000

    Add CRUD functionality

--- pokedex-app/frontend/src/app/components/create-pokedex/create-pokedex.component.ts
+++ pokedex-app/frontend/src/app/components/create-pokedex/create-pokedex.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { PokemonsService } from 'src/app/services/pokemons.service';
 import { Pokedex } from 'src/interfaces/pokedex.interface';
 
 @Component({
@@ -11,16 +12,21 @@ import { Pokedex } from 'src/interfaces/pokedex.interface';
 })
 export class CreatePokedexComponent implements OnInit {
   pokedexForm: FormGroup
+  title = 'New Pokemon'
+  action = 'Add'
+  id: string | null
 
-  constructor(private fb: FormBuilder, private router: Router, private toastr: ToastrService) { 
+  constructor(private fb: FormBuilder, private router: Router, private toastr: ToastrService, private pokemonsService: PokemonsService, private aRouter: ActivatedRoute) { 
     this.pokedexForm = this.fb.group({
       name: ['', Validators.required],
       level: ['', Validators.required],
       favourite: [false, Validators.required]
     })
+    this.id = this.aRouter.snapshot.paramMap.get('id')
   }
 
   ngOnInit(): void {
+    this.updatePokemon()
   }
 
   addPokemon() {
@@ -29,8 +35,39 @@ export class CreatePokedexComponent implements OnInit {
       level: this.pokedexForm.get('level')?.value,
       favourite: this.pokedexForm.get('favourite')?.value
     }
-    this.toastr.success('Pokemon added successfully')
-    this.router.navigate(['/pokedex'])
+
+    if(this.id !== null) {
+      // Update Pokemon
+      const obs$ = this.pokemonsService.updatePokemon(this.id, pokemon).subscribe((data) => {
+        this.toastr.info('Pokemon updated successfully')
+        this.router.navigate(['/pokedex'])
+        this.pokedexForm.reset()
+        obs$.unsubscribe()
+      })
+    } else {
+      // Create Pokemon
+      const obs$ = this.pokemonsService.createPokemon(pokemon).subscribe((data) => {
+        this.toastr.success('Pokemon added successfully')
+        this.router.navigate(['/pokedex'])
+        this.pokedexForm.reset()
+        obs$.unsubscribe()
+      })
+    }
   }
 
+  updatePokemon() {
+    if(this.id !== null) {
+      this.title = 'Update Pokemon'
+      this.action = 'Edit'
+      const obs$ = this.pokemonsService.getPokemonById(this.id).subscribe((data) => {
+        this.pokedexForm.setValue({
+          name: data.name,
+          level: data.level,
+          favourite: data.favourite 
+        })
+        obs$.unsubscribe()
+      })
+    }
+
+  }
 }
